e74462a3d292dd7f96c48020f9b4a340cdce57f5
jaypatel200010 <jaypatel200010@gmail.com>
2023-12-13 21:00:11 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Main from "./Components/Main";
+import Aboutus from "./Components/Aboutus";
 
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar></Navbar>
+      {/* <Aboutus></Aboutus> */}
+      {/* <Main></Main> */}
+      <Router>
+        <Switch>
+          <Route path="/Aboutus">
+            <Aboutus />
+          </Route>
+          <Route path="/">
+            <Main />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
